74cd55f98b7193d298ab8c532df06d4aa37d8d5d
Maria <maria150@mail.ru>
2020-04-08 19:54:06 +0300

    script check

--- messenger_app/application/scriptDB.py
+++ messenger_app/application/scriptDB.py
@@ -1,5 +1,5 @@
-from django.conf import settings
-settings.configure(DEBUG=True)
+#from django.conf import settings
+#settings.configure(DEBUG=True)
 
 from user.models import User
 from chats.user_factory import RandomUserFactory
