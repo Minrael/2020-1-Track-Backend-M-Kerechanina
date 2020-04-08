af34bd6e7725c5ef98814ddf72282f541190a364
Maria <maria150@mail.ru>
2020-04-08 17:41:00 +0300

    venv/ del

--- messenger/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/maria/Projects/2020-1-Track-Backend-M-Kerechanina/messenger/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
